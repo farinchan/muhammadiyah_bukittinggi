ace41c38ea025dd03ec39a401e12523221ae2712
fajri <fajri@gariskode.com>
2024-09-01 10:42:52 +0700

    update

--- resources/views/back/app.blade.php
+++ resources/views/back/app.blade.php
@@ -4,29 +4,24 @@
 
 <head>
     <base href="" />
-    <title>Metronic - The World's #1 Selling Tailwind CSS & Bootstrap Admin Template by KeenThemes</title>
+    <title>Pimpinan Daerah Muhammadiyah Bukittinggi</title>
     <meta charset="utf-8" />
     <meta name="description"
         content="
-            The most advanced Tailwind CSS & Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo,
-            Dark Mode, RTL support and complete React, Angular, Vue, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js, Node.js, Flask, Symfony & Laravel versions.
-            Grab your copy now and get life-time updates for free.
+            Pimpinan Daerah Muhammadiyah Bukittinggi - Administration
         " />
     <meta name="keywords"
         content="
-            tailwind, tailwindcss, metronic, bootstrap, bootstrap 5, angular, VueJs, React, Asp.Net Core, Rails, Spring, Blazor, Django, Express.js,
-            Node.js, Flask, Symfony & Laravel starter kits, admin themes, web design, figma, web development, free templates,
-            free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button,
-            bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon
+            Pimpinan Daerah Muhammadiyah Bukittinggi, Pimpinan Daerah Muhammadiyah Bukittinggi - Administration
         " />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta property="og:locale" content="en_US" />
+    {{-- <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="article" />
     <meta property="og:title"
         content="Metronic - The World's #1 Selling Tailwind CSS & Bootstrap Admin Template by KeenThemes" />
     <meta property="og:url" content="https://keenthemes.com/metronic" />
     <meta property="og:site_name" content="Metronic by Keenthemes" />
-    <link rel="canonical" href="http://preview.keenthemes.com?page=index" />
+    <link rel="canonical" href="http://preview.keenthemes.com?page=index" /> --}}
     <link rel="shortcut icon" href="{{ asset('back/media/logos/favicon.ico')}}" />
     <!--begin::Fonts(mandatory for all pages)-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" /> <!--end::Fonts-->
