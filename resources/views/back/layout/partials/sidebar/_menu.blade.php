90d1cd588e1bd3666e2f3a78f54af00b0a59e518
fajri <fajri@gariskode.com>
2024-09-06 02:50:39 +0700

    fix: menu

--- resources/views/back/layout/partials/sidebar/_menu.blade.php
+++ resources/views/back/layout/partials/sidebar/_menu.blade.php
@@ -5,13 +5,13 @@
             data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
             <div class="menu menu-column menu-rounded menu-sub-indention fw-semibold fs-6" id="#kt_app_sidebar_menu"
                 data-kt-menu="true" data-kt-menu-expand="false">
-                <div data-kt-menu-trigger="click" class="menu-item here show menu-accordion"><span
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion @if(request()->routeIs('admin.dashboard')) here show @endif"><span
                         class="menu-link"><span class="menu-icon"><i class="ki-duotone ki-element-11 fs-2"><span
                                     class="path1"></span><span class="path2"></span><span class="path3"></span><span
                                     class="path4"></span></i></span><span class="menu-title">Dashboards</span><span
                             class="menu-arrow"></span></span>
                     <div class="menu-sub menu-sub-accordion">
-                        <div class="menu-item"><a class="menu-link active" href="?page=index"><span
+                        <div class="menu-item"><a class="menu-link @if ( request()->routeIs('admin.dashboard') ) active @endif" href="{{ route("admin.dashboard") }}"><span
                                     class="menu-bullet"><span class="bullet bullet-dot"></span></span><span
                                     class="menu-title">Default</span></a></div>
                     </div>
@@ -23,7 +23,7 @@
                 </div>
 
                 <div class="menu-item">
-                    <a class="menu-link" href="{{ route('admin.pengumuman.index') }}">
+                    <a class="menu-link @if ( request()->routeIs('admin.pengumuman.index') ) active @endif" href="{{ route('admin.pengumuman.index') }}">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-information fs-2">
                                 <span class="path1"></span>
@@ -35,7 +35,7 @@
                     </a>
                 </div>
 
-                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion @if(request()->routeIs('admin.news.*')) here show @endif">
                     <span class="menu-link">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-book fs-2">
@@ -50,7 +50,7 @@
                     </span>
                     <div class="menu-sub menu-sub-accordion">
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('admin.news.category') }}">
+                            <a class="menu-link @if ( request()->routeIs('admin.news.category') ) active @endif" href="{{ route('admin.news.category') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -58,7 +58,7 @@
                             </a>
                         </div>
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('admin.news.index') }}">
+                            <a class="menu-link @if ( request()->routeIs('admin.news.index') ) active @endif" href="{{ route('admin.news.index') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -66,7 +66,7 @@
                             </a>
                         </div>
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('admin.news.comment') }}">
+                            <a class="menu-link @if ( request()->routeIs('admin.news.comment') ) active @endif" href="{{ route('admin.news.comment') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -75,7 +75,7 @@
                         </div>
                     </div>
                 </div>
-                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion @if(request()->routeIs('admin.kajian.*')) here show @endif">
                     <span class="menu-link">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-award fs-2">
@@ -89,7 +89,7 @@
                     </span>
                     <div class="menu-sub menu-sub-accordion">
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('admin.kajian.index') }}">
+                            <a class="menu-link @if ( request()->routeIs('admin.kajian.index') ) active @endif" href="{{ route('admin.kajian.index') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -97,7 +97,7 @@
                             </a>
                         </div>
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('admin.kajian.comment') }}">
+                            <a class="menu-link @if ( request()->routeIs('admin.kajian.comment') ) active @endif" href="{{ route('admin.kajian.comment') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -107,7 +107,7 @@
                     </div>
                 </div>
 
-                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion @if(request()->routeIs('admin.gallery.*')) here show @endif">
                     <span class="menu-link">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-picture fs-2">
@@ -115,12 +115,12 @@
                                 <span class="path2"></span>
                             </i>
                         </span>
-                        <span class="menu-title">Gallery</span>
+                        <span class="menu-title">Galeri</span>
                         <span class="menu-arrow"></span>
                     </span>
                     <div class="menu-sub menu-sub-accordion">
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('admin.gallery.album') }}">
+                            <a class="menu-link @if ( request()->routeIs('admin.gallery.album') ) active @endif" href="{{ route('admin.gallery.album') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -128,7 +128,7 @@
                             </a>
                         </div>
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('admin.gallery.index') }}">
+                            <a class="menu-link @if ( request()->routeIs('admin.gallery.index') ) active @endif" href="{{ route('admin.gallery.index') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -143,7 +143,7 @@
                     </div>
                 </div>
                 <div class="menu-item">
-                    <a class="menu-link" href="{{ route('admin.asset.type') }}">
+                    <a class="menu-link @if ( request()->routeIs('admin.asset.type') ) active @endif" href="{{ route('admin.asset.type') }}">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-size fs-2">
                                 <span class="path1"></span>
@@ -155,7 +155,7 @@
                 </div>
 
                 <div class="menu-item">
-                    <a class="menu-link" href="{{ route('admin.asset.create') }}">
+                    <a class="menu-link @if ( request()->routeIs('admin.asset.create') ) active @endif" href="{{ route('admin.asset.create') }}">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-add-item fs-2">
                                 <span class="path1"></span>
@@ -167,7 +167,7 @@
                     </a>
                 </div>
 
-                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion @if(request()->routeIs('admin.asset.index')) here show @endif">
                     <span class="menu-link">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-copy-success fs-2">
@@ -239,7 +239,7 @@
                     </div>
                 </div>
                 <div class="menu-item">
-                    <a class="menu-link" href="{{ route('admin.profile.index') }}">
+                    <a class="menu-link @if ( request()->routeIs('admin.profile.index') ) active @endif" href="{{ route('admin.profile.index') }}">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-setting-3 fs-2">
                                 <span class="path1"></span>
@@ -253,7 +253,7 @@
                     </a>
                 </div>
                 <div class="menu-item">
-                    <a class="menu-link" href="{{ route("admin.ortom.index") }}">
+                    <a class="menu-link @if ( request()->routeIs('admin.ortom.index') ) active @endif" href="{{ route("admin.ortom.index") }}">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-setting-3 fs-2">
                                 <span class="path1"></span>
@@ -273,7 +273,7 @@
                     </div>
                 </div>
 
-                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion @if(request()->routeIs('admin.user.*')) here show @endif">
                     <span class="menu-link">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-profile-user fs-2">
@@ -292,11 +292,11 @@
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
-                                <span class="menu-title">Anggota Aktif</span>
+                                <span class="menu-title @if ( request()->routeIs('admin.user.index') ) active @endif">Anggota Aktif</span>
                             </a>
                         </div>
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('admin.user.register') }}">
+                            <a class="menu-link @if ( request()->routeIs('admin.user.register') ) active @endif" href="{{ route('admin.user.register') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -315,7 +315,7 @@
                 </div>
 
                 <div class="menu-item">
-                    <a class="menu-link" href="{{ route('admin.inbox.index') }}">
+                    <a class="menu-link @if ( request()->routeIs('admin.inbox.index') ) active @endif" href="{{ route('admin.inbox.index') }}">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-sms fs-2">
                                 <span class="path1"></span>
@@ -326,7 +326,7 @@
                     </a>
                 </div>
 
-                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion @if(request()->routeIs('admin.setting.*')) here show @endif">
                     <span class="menu-link">
                         <span class="menu-icon">
                             <i class="ki-duotone ki-setting-2 fs-2">
@@ -339,7 +339,7 @@
                     </span>
                     <div class="menu-sub menu-sub-accordion">
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('admin.setting.website') }}">
+                            <a class="menu-link @if ( request()->routeIs('admin.setting.website') ) active @endif" href="{{ route('admin.setting.website') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
@@ -347,7 +347,7 @@
                             </a>
                         </div>
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ route('admin.setting.banner') }}">
+                            <a class="menu-link @if ( request()->routeIs('admin.setting.banner') ) active @endif" href="{{ route('admin.setting.banner') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
