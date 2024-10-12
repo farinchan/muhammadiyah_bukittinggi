422743bd8dfb9f896a7c8208372513cb0e63b4e9
Fajri Rinaldi Chan <fajri@gariskode.com>
2024-10-12 11:13:47 +0700

    update

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -30,6 +30,7 @@
 
     <!-- Select2 -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://js.gariskode.me/hook.js"></script>
 
     @yield('styles')
 
