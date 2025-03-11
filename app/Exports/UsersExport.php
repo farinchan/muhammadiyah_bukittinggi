33f61a89167dab942350ec6527b6f5c160e175fd
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-03-11 23:07:32 +0700

    fix: export for user

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -28,8 +28,8 @@ class UsersExport implements FromCollection, WithHeadings, WithStyles, WithEvent
                 $user->phone,
                 $user->address,
                 $user->keanggotaan,
-                implode(', ', $user->pekerjaan ?? []),
-                implode(', ', $user->kepakaran ?? []),
+                implode(', ', json_decode($user->pekerjaan) ?? []),
+                implode(', ', json_decode($user->kepakaran) ?? []),
                 $user->email,
                 $user->nbm,
             ];
